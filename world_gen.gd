b2c294c0c2a33327c5a606b7ad47b1b8c1cf0819
Engl Rasmus <rasencraft11@gmail.com>
2026-01-15 15:30:10 +0100

    add player character and 2 tile types

--- world_gen.gd
+++ world_gen.gd
@@ -1,12 +1,19 @@
 extends Node
 @onready var map = $GridMap
 @onready var cell_pos = Vector3i(0,0,0)
+@onready var noise = FastNoiseLite.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	noise.get_image(100,100,)
 	for x in range(1,200):
 		for i in range(1,200):
 			cell_pos = Vector3i(x,0,i)
-			map.set_cell_item(cell_pos,0)
+			if noise.get_noise_2d(x,i) < 0.2:
+				map.set_cell_item(cell_pos,0)
+			else:
+				map.set_cell_item(cell_pos,1)
+			
 
 	pass # Replace with function body.
 
