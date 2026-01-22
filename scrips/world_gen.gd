8a99ff48e659b798ba5671ed5bb0bf2b1c63f377
Engl Rasmus <rasencraft11@gmail.com>
2026-01-22 14:39:18 +0100

    rebase code

--- scrips/world_gen.gd
+++ scrips/world_gen.gd
@@ -5,18 +5,22 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	noise.get_image(100,100,)
-	for x in range(1,200):
-		for i in range(1,200):
+	generate_map(1000,1000)
+
+	pass # Replace with function body.
+func generate_map(with: int, height: int):
+	noise.get_image(with,height,)
+	for x in range(1,with):
+		for i in range(1,height):
 			cell_pos = Vector3i(x,0,i)
 			if noise.get_noise_2d(x,i) < 0.2:
 				map.set_cell_item(cell_pos,0)
 			else:
 				map.set_cell_item(cell_pos,1)
-			
-
-	pass # Replace with function body.
-
+	
+# func remove_map():
+	
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
