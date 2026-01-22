1548ffa8eb7eec183cad8e2eef1c7f2385d02cd6
Engl Rasmus <rasencraft11@gmail.com>
2026-01-22 15:28:08 +0100

    world gen infinite

--- scrips/world_gen.gd
+++ scrips/world_gen.gd
@@ -2,27 +2,43 @@ extends Node
 @onready var map = $GridMap
 @onready var cell_pos = Vector3i(0,0,0)
 @onready var noise = FastNoiseLite.new()
-
+var move = Vector2i(0,0)
+var mapSize= Vector2i(100,100)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	generate_map(1000,1000)
-
+	generate_map(mapSize.x,mapSize.y, move.x,move.y)
 	pass # Replace with function body.
-func generate_map(with: int, height: int):
-	noise.get_image(with,height,)
+	
+func generate_map(with: int, height: int, movx,movy):
+	noise.get_image(with,height)
+	noise.offset.x = movx
 	for x in range(1,with):
 		for i in range(1,height):
 			cell_pos = Vector3i(x,0,i)
 			if noise.get_noise_2d(x,i) < 0.2:
-				map.set_cell_item(cell_pos,0)
-			else:
 				map.set_cell_item(cell_pos,1)
-	
-# func remove_map():
+			else:
+				map.set_cell_item(cell_pos,2)
+
+func remove_map():
+	map.clear()
 	
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var dir = Input.get_axis("ui_left","ui_right")
+	print(dir)
+	if dir >= 0:
+		move.x -=1
+		print(move.x)
+		remove_map()
+		generate_map(mapSize.x,mapSize.y,move.x,move.y)
+	if dir <= 0:
+		move.x +=1
+		print(move.x)
+		remove_map()
+		generate_map(mapSize.x,mapSize.y,move.x,move.y)
+	
 	
 	pass
