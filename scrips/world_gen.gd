48604f9101bd0659585a79b1801b94b6323e22ae
Rasmus Engl <rasencraft11@gmail.com>
2026-01-26 20:19:53 +0100

    add branch
    add branch and multiple test files to work on worldgeneration

--- scrips/world_gen.gd
+++ scrips/world_gen.gd
@@ -4,22 +4,33 @@ extends Node
 @onready var noise = FastNoiseLite.new()
 var move = Vector2i(0,0)
 var mapSize= Vector2i(100,100)
+var circlepos = Vector2i(0,0)
+var Renderdistance = 10
+var circle = Geometry2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	generate_map(mapSize.x,mapSize.y, move.x,move.y)
+	generate_map(mapSize.x,mapSize.y, move.x,move.y,"A")
 	pass # Replace with function body.
 	
-func generate_map(with: int, height: int, movx,movy):
+
+func generate_map(with: int, height: int, movx,movy,type: String):
 	noise.get_image(with,height)
 	noise.offset.x = movx
 	noise.offset.y = movy
 	for x in range(1,with):
 		for i in range(1,height):
 			cell_pos = Vector3i(x,0,i)
-			if noise.get_noise_2d(x,i) < 0.5:
-				map.set_cell_item(cell_pos,1)
+			if type == "1":
+				if circle.is_point_in_circle(Vector2(x,i),circlepos,Renderdistance) == true:
+					if noise.get_noise_2d(x,i) < 0.3:
+						map.set_cell_item(cell_pos,1)
+					else:
+						map.set_cell_item(cell_pos,2)
 			else:
-				map.set_cell_item(cell_pos,2)
+				if noise.get_noise_2d(x,i) < 0.3:
+					map.set_cell_item(cell_pos,1)
+				else:
+					map.set_cell_item(cell_pos,2)
 
 func remove_map():
 	map.clear()
@@ -32,24 +43,21 @@ func _process(delta: float) -> void:
 	var diry = Input.get_axis("ui_up","ui_down")
 	if dirx >= 0:
 		move.x -=1
-		
 		remove_map()
-		generate_map(mapSize.x,mapSize.y,move.x,move.y)
+		generate_map(mapSize.x,mapSize.y,move.x,move.y,"A")
 	if dirx <= 0:
 		move.x +=1
-		
 		remove_map()
-		generate_map(mapSize.x,mapSize.y,move.x,move.y)
+		generate_map(mapSize.x,mapSize.y,move.x,move.y,"A")
 	if diry >= 0:
 		move.y -=1
-		
 		remove_map()
-		generate_map(mapSize.x,mapSize.y,move.x,move.y)
+		generate_map(mapSize.x,mapSize.y,move.x,move.y,"A")
 	if diry <= 0:
 		move.y +=1
 		
 		remove_map()
-		generate_map(mapSize.x,mapSize.y,move.x,move.y)
+		generate_map(mapSize.x,mapSize.y,move.x,move.y,"A")
 	
 	
 	pass
